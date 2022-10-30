9f41525ad03014b51849e71bea3b4c0ccc266244
Priyan <priyanjeyaram.9_it@mepcoeng.ac.in>
2022-10-31 00:02:09 +0530

    TabLayout Implemented

---
+++ app/src/main/java/com/example/smartportal/MarksActivity.java
@@ -0,0 +1,48 @@
+package com.example.smartportal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MarksActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marks);
+
+        tabLayout= findViewById(R.id.tabLayout);
+        viewPager= findViewById(R.id.viewPager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("MidTerm"));
+        tabLayout.addTab(tabLayout.newTab().setText("Revision"));
+        tabLayout.addTab(tabLayout.newTab().setText("Terminal"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/smartportal/MyAdapter.java
@@ -0,0 +1,39 @@
+package com.example.smartportal;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyAdapter extends FragmentPagerAdapter {
+
+    private Context myContext;
+    int totalTabs;
+
+    public MyAdapter(Context context, FragmentManager fm, int totalTabs) {
+        super(fm);
+        myContext = context;
+        this.totalTabs = totalTabs;
+    }
+
+    // this is for fragment tabs
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new MidtermFragment();
+            case 1:
+                return new RevisionFragment();
+            case 2:
+                return new TerminalFragment();
+            default:
+                return null;
+        }
+    }
+    // this counts total number of tabs
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
